24b4b6460b66d51d3091d8d9667f113d85e8c18c
RojilaMainali <rojilamainali@gmail.com>
2021-12-20 11:49:48 +0545

    added repository

--- ApexRestaurant.Repository/RepositoryModule.cs
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ApexRestaurant.Repository.RCustomer;
+using ApexRestaurant.Repository.RMeal;
+using ApexRestaurant.Repository.RMenu;
+using ApexRestaurant.Repository.RMenu_Item;
+using ApexRestaurant.Repository.RRef_Staff_Role;
+using ApexRestaurant.Repository.RStaff;
 namespace ApexRestaurant.Repository
 {
     public static class RepositoryModule
@@ -9,6 +14,12 @@ namespace ApexRestaurant.Repository
         {
             services.AddDbContext<RestaurantContext>(options => options.UseMySQL(connection, builder => builder.MigrationsAssembly(migrationsAssembly)));
             services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IMealRepository, MealRepository>();
+            //services.AddTransient<IMeal_DishesRepository, Meal_DishesRepository>();
+            services.AddTransient<IMenuRepository, MenuRepository>();
+            services.AddTransient<IMenu_ItemRepository, Menu_ItemRepository>();
+            services.AddTransient<IRef_Staff_RoleRepository, Ref_Staff_RoleRepository>();
+            services.AddTransient<IStaffRepository,StaffRepository>();
         }
     }
 }
--- ApexRestaurant.Repository/RestaurantContext.cs
+++ ApexRestaurant.Repository/RestaurantContext.cs
@@ -11,6 +11,13 @@ base(options)
  }
  public DbSet<Customer> Customers { get; set; }
  
+ public DbSet<Meal> Meals { get; set; }
+ public DbSet<Menu_Item> Menu_Items { get; set; }
+ public DbSet<Menu> Menus { get; set; }
+ public DbSet<Ref_Staff_Role> Ref_Staff_Roles { get; set; }
+ public DbSet<Staff> Staffs { get; set; }
+
+ 
  protected override void OnModelCreating(ModelBuilder modelBuilder)
  {
  base.OnModelCreating(modelBuilder);
